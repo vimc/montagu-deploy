883777bdaefd903cfdd99c255d517eecd5a17a38
Rich FitzJohn <r.fitzjohn@imperial.ac.uk>
2020-02-03 10:57:20 +0000

    Basic configuration

--- src/config.py
+++ src/config.py
@@ -7,9 +7,51 @@ class MontaguConfig:
         dat = config.read_yaml("{}/montagu.yml".format(path))
         dat = config.config_build(path, dat, config_name)
 
-        self.proxy_host = config.config_string(dat, ["proxy", "host"])
-        self.proxy_port = config.config_integer(dat, ["proxy", "port"])
+        self.proxy_host = config.config_string(
+            dat, ["proxy", "host"])
+        self.proxy_port = config.config_integer(
+            dat, ["proxy", "port"])
         self.proxy_ssl_certificate = config.config_string(
             dat, ["proxy", "ssl", "certificate"], True)
         self.proxy_ssl_key = config.config_string(
             dat, ["proxy", "ssl", "key"], True)
+
+        self.db_persist = config.config_boolean(
+            dat, ["db", "persist"])
+        self.db_backup = config.config_boolean(
+            dat, ["db", "backup"])
+        self.db_initial = config.config_enum(
+            dat, ["db", "initial"], ["minimal", "test", "restore"])
+        self.db_update = config.config_boolean(
+            dat, ["db", "update"])
+        self.db_replication = config.config_boolean(
+            dat, ["db", "replication"])
+        self.db_large_memory = config.config_boolean(
+            dat, ["db", "large_memory"])
+        self.db_password_group = config.config_enum(
+            dat, ["db", "password_group"], ["production", "science", "fake"])
+
+        # TODO: validation for update
+
+        self.annex_type = config.config_enum(
+            dat, ["annex", "type"], ["fake", "readonly", "real"])
+
+        self.api_add_test_user = config.config_boolean(
+            dat, ["api", "add_test_user"])
+
+        self.vault = config.config_vault(dat, ["vault"])
+
+        self.docker_network = config.config_string(dat, ["docker", "network"])
+        self.docker_prefix = config.config_string(dat, ["docker", "prefix"])
+
+        self.general_open_browser = config.config_boolean(
+            dat, ["general", "open_browser"])
+        self.general_instance_name = config.config_string(
+            dat, ["general", "instance_name"])
+        self.general_notify_slack = config.config_boolean(
+            dat, ["general", "notify_slack"])
+
+        self.static_copy_static = config.config_boolean(
+            dat, ["static", "copy_static"])
+        self.static_copy_guidance = config.config_boolean(
+            dat, ["static", "copy_guidance"])
--- test/test_config.py
+++ test/test_config.py
@@ -1,9 +1,33 @@
 from src import config
 
 
-def test_default_proxy_configuration():
+def test_default_configuration():
     cfg = config.MontaguConfig("config")
     assert cfg.proxy_host == "localhost"
     assert cfg.proxy_port == 8443
     assert cfg.proxy_ssl_certificate is None
     assert cfg.proxy_ssl_key is None
+
+    assert not cfg.db_persist
+    assert not cfg.db_backup
+    assert cfg.db_initial == "minimal"
+    assert not cfg.db_update
+    assert not cfg.db_replication
+    assert not cfg.db_large_memory
+    assert cfg.db_password_group == "fake"
+
+    assert cfg.annex_type == "fake"
+
+    assert not cfg.api_add_test_user
+
+    assert not cfg.vault.url
+
+    assert cfg.docker_network == "montagu_nw"
+    assert cfg.docker_prefix == "montagu"
+
+    assert not cfg.general_open_browser
+    assert cfg.general_instance_name == "teamcity"
+    assert not cfg.general_notify_slack
+
+    assert not cfg.static_copy_static
+    assert not cfg.static_copy_guidance
